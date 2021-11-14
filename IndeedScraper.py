b65cf94ac094aac6c00ccc8cc21c1b81cc4fab7a
Farooq Akhtar <farooq.akhtar100@gmail.com>
2021-11-14 01:57:38 -0800

    .

--- IndeedScraper.py
+++ IndeedScraper.py
@@ -1,3 +1,17 @@
-# hello this is the scraper
+from selenium import webdriver
+import time
+import pyautogui
+import re
+import keyboard
 
-print("hi")
+driver = webdriver.Chrome(executable_path=r'C:\webdrivers\chromedriver.exe')
+driver.get('https://myjobs.indeed.com/applied?hl=en&co=US&from=_atweb_gnav-homepage')
+
+input("Input to start: ")
+
+for i in range(100):
+    print(".")
+
+# do everthing you need to until you're logged in and ready
+
+#while True:
